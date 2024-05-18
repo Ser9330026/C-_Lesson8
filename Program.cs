7c263ea7a07772ac5c6ec1a3a4103663521feb2b
ser9330026 <ser9330026@yandex.ru>
2024-05-18 22:00:51 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Lesson8
+{
+    internal class Program
+    {
+        static int Diference(int height, int width)
+        {
+            return height - width;  
+        }
+
+        static void Main(string[] args)
+        {
+            int deference = Diference(10, 5);
+            Console.WriteLine(deference);
+
+            int deference2 = Diference(width: 10, height: 5);
+            Console.WriteLine(deference2);
+        }
+    }
+}
